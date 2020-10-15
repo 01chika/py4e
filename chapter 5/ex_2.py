ee8fe97382b9c90288d053aba1bc25749ef8e888
chika-dueke-eze <64172430+chika-dueke-eze@users.noreply.github.com>
2020-10-15 22:19:01 +0100

    Add files via upload

---
+++ chapter 5/ex_2.py
@@ -0,0 +1,30 @@
+aa = input ('Enter 1st number: ')
+a = int(aa)
+bb = input ('Enter 2nd number: ')
+b = int(bb)
+cc = input ('Enter 3rd number: ')
+c = int(cc)
+
+prompt = [a, b, c] 
+
+x_max= max(prompt)
+print ('Max:', x_max)
+x_min= min(prompt)
+print ('Min:', x_min)
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
